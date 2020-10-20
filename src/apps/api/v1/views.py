2130b52af05b67c0616f3ed269f63b50b6aecf35
Saiprasad Balasubramanian <sai.cosmo@gmail.com>
2020-10-21 01:07:25 +0530

    :sparkles: Implemented basic full-text search on the endpoint

--- src/apps/api/v1/views.py
+++ src/apps/api/v1/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+import django_filters
+from rest_framework import viewsets, filters
 
 from apps.api.v1.filters.movies_filter import MoviesFilter
 from apps.api.v1.serializers.serializers import MovieSerializer
@@ -9,5 +10,7 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = MovieInfo.objects.all().order_by('-imdb_score')
     serializer_class = MovieSerializer
     filterset_class = MoviesFilter
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['name']
 
     # permission_classes = [permissions.IsAuthenticated]
