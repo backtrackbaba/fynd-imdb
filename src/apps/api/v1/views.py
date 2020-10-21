186840a549e94bc3d0a81211372d438cad79ced8
Saiprasad Balasubramanian <sai.cosmo@gmail.com>
2020-10-21 16:28:20 +0530

    :lock: Implemented custom permission to make model only editable by admin

--- src/apps/api/v1/views.py
+++ src/apps/api/v1/views.py
@@ -2,15 +2,15 @@ import django_filters
 from rest_framework import viewsets, filters
 
 from apps.api.v1.filters.movies_filter import MoviesFilter
+from apps.api.v1.permissions.movie_permission import CustomIsAuthenticatedOrReadOnly
 from apps.api.v1.serializers.serializers import MovieSerializer
 from apps.core.models import MovieInfo
 
 
 class MovieViewSet(viewsets.ModelViewSet):
+    permission_classes = [CustomIsAuthenticatedOrReadOnly]
     queryset = MovieInfo.objects.all().order_by('-imdb_score')
     serializer_class = MovieSerializer
     filterset_class = MoviesFilter
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['name']
-
-    # permission_classes = [permissions.IsAuthenticated]
