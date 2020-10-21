2d167916648aeba2da16311673de3940a9f0b864
Saiprasad Balasubramanian <sai.cosmo@gmail.com>
2020-10-21 13:46:03 +0530

    :zap: Added new domain to host

--- src/config/settings.py
+++ src/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['imdb.saiprasad.io', 'localhost']
+ALLOWED_HOSTS = ['imdb.saiprasad.io', 'localhost', 'fynd.saiprasad.io']
 
 # Application definition
 
